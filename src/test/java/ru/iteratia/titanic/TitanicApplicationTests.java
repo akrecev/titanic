395a5193123e751d8af13945eb4d4f4bd1867a3f
akrecev <akrecev@gmail.com>
2024-07-06 19:21:58 +0300

    initial

---
+++ src/test/java/ru/iteratia/titanic/TitanicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.iteratia.titanic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitanicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
